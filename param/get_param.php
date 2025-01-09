0f5e9064386b497a6c021b22fca53112c413d223
yukinagata3184 <yuki.nagata3184@gmail.com>
2025-01-09 15:11:23 +0900

    [Add] #1 implemented a function to get parameters.

---
+++ param/get_param.php
@@ -0,0 +1,22 @@
+<?
+/**
+ * @file get_param.php
+ * @brief get dummy parameters.
+ * @author nagata
+ */
+
+/**
+ * @brief return the dummy salt.
+ * @retval [string] dummy salt.
+ */
+function getSalt(): string{
+    return 'h2Xib8jENmQve4dVXDeQ';
+}
+
+/**
+ * @brief return the dummy DB password.
+ * @retval [string] dummy DB password.
+ */
+function getDbPassword(): string{
+    return 'qpBSBwz4ac4wYUdznPRALaF2jygRZaTb';
+}
