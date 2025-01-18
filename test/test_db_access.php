c4c78fe999a602813bae8267b0e7d403cc1e0631
yukinagata3184 <yuki.nagata3184@gmail.com>
2025-01-18 18:26:21 +0900

    [Add] #16 getDbAll()でレコードとカラムが全件取得できていることを確認。

--- test/test_db_access.php
+++ test/test_db_access.php
@@ -7,10 +7,28 @@
 
 require_once __DIR__ . '/../logic/db_access.php';
 
-function testDbOpen(): void {
-    $dbh = dbOpen();
+/**
+ * @brief openDb()でPDOオブジェクトが取得できているかテスト。
+ */
+function testOpenDb(): void {
+    $dbh = openDb();
     $assertEqual = ((get_class($dbh) === 'PDO') ? 'passed' : 'failed');
     echo 'testGetSaltTrue : ' . $assertEqual . '<br>' . PHP_EOL;
 }
 
-testDbOpen();
+/**
+ * @brief getDbAll()で選択したテーブルからレコードとカラム全件を取得できているかテスト。
+ * @param $dbh [PDO] dbOpen()で取得したデータベースオブジェクトを指定。
+ * @param $tableName [string] 取得するテーブル名を指定。
+ */
+function testGetDbAll(PDO $dbh, string $tableName): void {
+    $allTable = getDbAll($dbh, $tableName);
+    echo 'testGetDbAll : ' . '<br>' . PHP_EOL;
+    echo ' テーブル名:' . $tableName . '<br>' . PHP_EOL;
+    echo ' レコード数:' . count($allTable) . '<br>' . PHP_EOL;
+    echo ' カラム数:' . count($allTable[0]) / count($allTable) . '<br>' . PHP_EOL;
+}
+
+testOpenDb();
+testGetDbAll(openDb(), 'm_product');
+testGetDbAll(openDb(), 'm_member');
