b36f4c5572df2147927ca38afcae5405c6136b1b
yukinagata3184 <yuki.nagata3184@gmail.com>
2025-01-18 14:33:31 +0900

    [Add] #14 getDbUser()の単体テスト

--- test/test_get_param.php
+++ test/test_get_param.php
@@ -10,36 +10,54 @@ require_once __DIR__ . '/../param/get_param.php';
 /**
  * @brief GetSalt()の戻り値と期待値が一致しているかテスト。
  */
-function testGetSaltTrue(): void{
+function testGetSaltTrue(): void {
     $assertEqual = ((getSalt() === 'h2Xib8jENmQve4dVXDeQ') ? 'passed' : 'failed');
-    echo 'testGetSalt : ' . $assertEqual . '<br>' . PHP_EOL;
+    echo 'testGetSaltTrue : ' . $assertEqual . '<br>' . PHP_EOL;
 }
 
 /**
  * @brief GetSalt()の戻り値が期待値以外と一致しないことをテスト。
  */
-function testGetSaltFalse(): void{
+function testGetSaltFalse(): void {
     $assertEqual = ((getSalt() === 'h2Xib8jENmQve4dVXDeA') ? 'failed' : 'passed');
-    echo 'testGetSalt : ' . $assertEqual . '<br>' . PHP_EOL;
+    echo 'testGetSaltFalse : ' . $assertEqual . '<br>' . PHP_EOL;
 }
 
 /**
  * @brief GetDbPassword()の戻り値と期待値が一致しているかテスト。
  */
-function testGetDbPasswordTrue(): void{
+function testGetDbPasswordTrue(): void {
     $assertEqual = ((getDbPassword() === 'qpBSBwz4ac4wYUdznPRALaF2jygRZaTb') ? 'passed' : 'failed');
-    echo 'getDbPassword : ' . $assertEqual . '<br>' . PHP_EOL;
+    echo 'getDbPasswordTrue : ' . $assertEqual . '<br>' . PHP_EOL;
 }
 
 /**
  * @brief GetDbPassword()の戻り値が期待値以外と一致しないことをテスト。
  */
-function testGetDbPasswordFalse(): void{
+function testGetDbPasswordFalse(): void {
     $assertEqual = ((getDbPassword() === 'qpBSBwz4ac4wYUdznPRALaF2jygRZaTa') ? 'failed' : 'passed');
-    echo 'getDbPassword : ' . $assertEqual . '<br>' . PHP_EOL;
+    echo 'getDbPasswordFalse : ' . $assertEqual . '<br>' . PHP_EOL;
+}
+
+/**
+ * @brief getDbUser()の戻り値と期待値が一致しているかテスト。
+ */
+function testGetDbUserTrue(): void {
+    $assertEqual = ((getDbUser() === 'admin') ? 'passed' : 'failed');
+    echo 'testGetSaltTrue : ' . $assertEqual . '<br>' . PHP_EOL;
+}
+
+/**
+ * @brief getDbUser()の戻り値が期待値以外と一致しないことをテスト。
+ */
+function testGetDbUserFalse(): void {
+    $assertEqual = ((getDbUser() === 'user') ? 'failed' : 'passed');
+    echo 'testGetSaltTrue : ' . $assertEqual . '<br>' . PHP_EOL;
 }
 
 testGetSaltTrue();
 testGetSaltFalse();
 testGetDbPasswordTrue();
 testGetDbPasswordFalse();
+testGetDbUserTrue();
+testGetDbUserFalse();
