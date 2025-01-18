073bc4540f4cfd948e1077f61b8049d4edd330dc
yukinagata3184 <yuki.nagata3184@gmail.com>
2025-01-18 14:18:27 +0900

    [Update] #14 期待値と一致しない場合のテストケースを追加

--- test/test_get_param.php
+++ test/test_get_param.php
@@ -1,27 +1,45 @@
 <?php
 /**
  * @file test_get_param.php
- * @brief test get_param.php.
+ * @brief get_param.php の単体テスト。
  * @author nagata
  */
 
 require_once __DIR__ . '/../param/get_param.php';
 
 /**
- * @brief Check to match the expected values at GetSalt().
+ * @brief GetSalt()の戻り値と期待値が一致しているかテスト。
  */
-function testGetSalt(): void{
+function testGetSaltTrue(): void{
     $assertEqual = ((getSalt() === 'h2Xib8jENmQve4dVXDeQ') ? 'passed' : 'failed');
     echo 'testGetSalt : ' . $assertEqual . '<br>' . PHP_EOL;
 }
 
 /**
- * @brief Check to match the expected values at GetDbPassword().
+ * @brief GetSalt()の戻り値が期待値以外と一致しないことをテスト。
  */
-function testGetDbPassword(): void{
+function testGetSaltFalse(): void{
+    $assertEqual = ((getSalt() === 'h2Xib8jENmQve4dVXDeA') ? 'failed' : 'passed');
+    echo 'testGetSalt : ' . $assertEqual . '<br>' . PHP_EOL;
+}
+
+/**
+ * @brief GetDbPassword()の戻り値と期待値が一致しているかテスト。
+ */
+function testGetDbPasswordTrue(): void{
     $assertEqual = ((getDbPassword() === 'qpBSBwz4ac4wYUdznPRALaF2jygRZaTb') ? 'passed' : 'failed');
     echo 'getDbPassword : ' . $assertEqual . '<br>' . PHP_EOL;
 }
 
-testGetSalt();
-testGetDbPassword();
+/**
+ * @brief GetDbPassword()の戻り値が期待値以外と一致しないことをテスト。
+ */
+function testGetDbPasswordFalse(): void{
+    $assertEqual = ((getDbPassword() === 'qpBSBwz4ac4wYUdznPRALaF2jygRZaTa') ? 'failed' : 'passed');
+    echo 'getDbPassword : ' . $assertEqual . '<br>' . PHP_EOL;
+}
+
+testGetSaltTrue();
+testGetSaltFalse();
+testGetDbPasswordTrue();
+testGetDbPasswordFalse();
